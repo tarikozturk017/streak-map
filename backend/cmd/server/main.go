d152e02da599d697ca102d04d190a8a498efc37c
tarikozturk017 <tarikozturk017@gmail.com>
2025-07-15 19:09:33 -0400

    graceful shutdown HTTP server

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,9 +1,10 @@
-package main
-
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/tarikozturk017/streak-map/backend/internal/auth"
@@ -55,8 +56,26 @@ func main() {
 	port := getEnv("PORT", "8080")
 	log.Printf("Server starting on port %s", port)
 	
-	if err := http.ListenAndServe(":"+port, mux); err != nil {
-		log.Fatal("Server failed to start:", err)
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown:", err)
 	}
 }
 
